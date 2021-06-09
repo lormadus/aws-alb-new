7db66458cf851f45053bceaab43e81b75e05e473
David Yoon <david.hyeshik@yahoo.com>
2021-06-09 17:45:05 +0900

    Update vm1.tf

--- vm1.tf
+++ vm1.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "vm1" {
   key_name          = var.dev_keyname
   vpc_security_group_ids = [
     aws_security_group.vm-sg.id,
-    aws_default_security_group.dev_default.id,
+    aws_default_security_group.dev_default.id
   ]
 
   subnet_id                   = aws_subnet.public_1a.id
